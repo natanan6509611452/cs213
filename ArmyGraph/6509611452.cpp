2e2c869ea238ff40b6a80166dc7042b255696124
Natanan Phopradit <natanan.pho@dome.tu.ac.th>
2023-12-06 00:10:00 +0700

    complete (without prompt and helper functions)

--- ArmyGraph/6509611452.cpp
+++ ArmyGraph/6509611452.cpp
@@ -18,11 +18,6 @@ public:
 		bool operator>(Position a) const{
 			return shortestFromOrigin > a.shortestFromOrigin;
 		}
-
-		bool operator==(const Position& other) const
-		{
-			return X == other.X && Y == other.Y && height == other.height && seen == other.seen && shortestFromOrigin == other.shortestFromOrigin;
-		}
 	};
 
 	ArmyAdvancement(int warZoneSize = 4, int targetNumber = 1, int knownHeightNumber = 0) : warZoneSize(warZoneSize), targetNumber(targetNumber), knownHeightNumber(knownHeightNumber) {
@@ -56,11 +51,9 @@ public:
 		delete[] targets;
 
 		delete origin;
-		cout << "ArmyAdvancement destructed";
 	}
 
 	void setUp() {
-		cout << "Enter war zone size, target number, known height number, origin position, and origin height (?,?,?,?,?,?)." << endl;
 		origin = new Position;
 		cin >> warZoneSize >> targetNumber >> knownHeightNumber >> origin->X >> origin->Y >> origin->height;
 		(origin->X)--;
@@ -172,33 +165,6 @@ public:
 		return slowestTroops;
 	}
 
-	void printDay() {
-		cout << "========== " << "Day(s) Required From Base" << " ==========" << endl;
-		for (int i = 0; i < warZoneSize; i++) {
-			for (int j = 0; j < warZoneSize; j++) {
-				cout << warZones[i][j]->shortestFromOrigin << " ";
-			}
-			cout << endl;
-		}
-	}
-
-	void printHeightMap() {
-		cout << "========== " << "War Zone Heights" << " ==========" << endl;
-		for (int i = 0; i < warZoneSize; i++) {
-			for (int j = 0; j < warZoneSize; j++) {
-				cout << warZones[i][j]->height << " ";
-			}
-			cout << endl;
-		}
-	}
-
-	void printTargets() {
-		cout << "========== " << "Targets" << " ==========" << endl;
-		for (int i = 0; i < targetNumber; i++) {
-			cout << "Targets at (" << targets[i]->X << ", " << targets[i]->Y << ") Height: " << targets[i]->height << endl;
-		}
-	}
-
 private:
 	int warZoneSize;
 	Position*** warZones;
@@ -211,20 +177,14 @@ private:
 
 int main(int argc, char** argv) {
 	ArmyAdvancement* firstStrategy = new ArmyAdvancement();
-	firstStrategy->printHeightMap();
 	int failedCount = firstStrategy->findFailed();
-	firstStrategy->printDay();
-	firstStrategy->printTargets();
 
 	if (failedCount > 0) {
-		cout << "========== " << "Troops Failed to Settle" << " ==========" << endl;
 		cout << failedCount << endl;
 	}
 	else {
-		cout << "========== " << "Most Day(s)" << " ==========" << endl;
 		ArmyAdvancement::Position* slowestTroops = firstStrategy->findSlowest();
 		cout << slowestTroops->shortestFromOrigin << endl;
-		cout << "========== " << "Least Day(s)" << " ==========" << endl;
 		ArmyAdvancement::Position* fastestTroop = firstStrategy->findFastest();
 		cout << fastestTroop->shortestFromOrigin << endl;
 	}
